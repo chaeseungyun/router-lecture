cb36174bcce7aaaec4f7b59bee6aa197a39efef1
ParkSungju01 <psj001002@koreatech.ac.kr>
2024-01-04 21:36:33 +0900

    11주차

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./Main";
 import Box from "./Box";
+import { useContext } from "react";
 
 function App() {
   return (
--- src/Box.jsx
+++ src/Box.jsx
@@ -1,12 +1,25 @@
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const ColorBox = styled.div`
   border: 1px solid black;
   width: 500px;
   height: 500px;
-  background-color: ${(props) => props.color || "black"};
+  background-color: ${(box_c) => box_c.color || "black"};
 `;
 
 export default function Box() {
-  return <ColorBox color="blue" />;
+  const param = useParams();
+  console.log(param);
+  const props = parseInt(param.number);
+  let box_c = "red";
+  if (props > 15) {
+    box_c = "blue";
+  } else if (props > 10) {
+    box_c = "green";
+  } else if (props > 5) {
+    box_c = "white";
+  }
+
+  return <ColorBox color={box_c} />;
 }
